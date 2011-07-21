1a0e51aa96b98dff3fb55083bad2302842347a31
René Six <mail@rene-six.de>
2011-07-22 01:59:56 +0200

    add color support within less

--- variable.sh
+++ variable.sh
@@ -115,10 +115,6 @@ export GREP_COLOR="31;01" #}}}
 # additional options for grep
 export GREP_OPTIONS="--color=auto" #}}}
 
-# {{{ LESS
-# Options which are passed to less automatically.
-export LESS="" #}}}
-
 # {{{ LESSCHARSET
 # Selects a predefined character set.
 export LESSCHARSET="UTF-8" #}}}
@@ -130,6 +126,16 @@ export LESSCHARSET="UTF-8" #}}}
 # DOS and Windows systems, or "$HOME/lesshst.ini" or "$INIT/lesshst.ini" on OS/2
 # systems.
 export LESSHISTFIE="-" #}}}
+
+# {{{ color in less
+export LESS_TERMCAP_mb=$'\E[01;31m'
+export LESS_TERMCAP_md=$'\E[01;31m'
+export LESS_TERMCAP_me=$'\E[0m'
+export LESS_TERMCAP_se=$'\E[0m'
+export LESS_TERMCAP_so=$'\E[01;44;33m'
+export LESS_TERMCAP_ue=$'\E[0m'
+export LESS_TERMCAP_us=$'\E[01;32m']
+#}}}
 
 # {{{ CDDB_PATH
 # settings for mcdp
