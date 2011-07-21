a3cb525f497f24150bb795c7abaac057cd442116
René Six <mail@rene-six.de>
2011-07-22 01:59:04 +0200

    remove all functions from shell/function.sh and add purge functions shameless stolen from grml

--- function.sh
+++ function.sh
@@ -1,54 +1,29 @@
-# {{{ uni-vpn
-function vpn-uni (){
-    if [[ ${1} == "open" ]] ; then
-        sudo vpnc ~/Uni-Marburg/vpn.zug
-        sudo route add default gw cvrzrz0102.hrz.uni-marburg.de dev tun0
-    elif [[ ${1} == "close" ]] ; then
-        sudo route del default gw cvrzrz0102.hrz.uni-marburg.de dev tun0
-        sudo vpnc-disconnect
-    else
-        echo -e "usage: ${0} open/ ${0} close"
-    fi
-} #}}}
-
-# {{{ clean unnessesary latex output file
-function latex-clean (){
-    find . -name '*.aux' -delete
-    find . -name '*.bak' -delete
-    find . -name '*.bbl' -delete
-    find . -name '*.bib' -delete
-    find . -name '*.blg' -delete
-    find . -name '*.fls' -delete
-    find . -name '*.idx' -delete
-    find . -name '*.ilg' -delete
-    find . -name '*.ind' -delete
-    find . -name '*.log' -delete
-    find . -name '*.nav' -delete
-    find . -name '*.out' -delete
-    find . -name '*.snm' -delete
-    find . -name '*.toc' -delete
-} #}}}
-
-# {{{ clean git tree
-function gitclean() {
-    if [ -d .git ] ; then
-        git gc
-
-        find . -type f -name '*.orig' -delete
-        find . -type f -name '*.BASE.*' -delete
-        find . -type f -name '*.LOCAL.*' -delete
-        find . -type f -name '*.REMOTE.*' -delete
-
-        git fsck --unreachable
-        git fsck --full
-        git prune
-
-        if [ -r .gitignore ] ; then
-            while read line ; do
-                find . -type f -name "${line}" -delete
-            done < .gitignore
+# {{{ Clean up directory - remove well known tempfiles
+purge() {
+    emulate -L zsh
+    setopt HIST_SUBST_PATTERN
+    local -a TEXTEMPFILES GHCTEMPFILES PYTEMPFILES FILES
+    TEXTEMPFILES=(*.tex(N:s/%tex/'(log|toc|aux|nav|snm|out|tex.backup|bbl|blg|bib.backup|vrb|lof|lot|hd|idx)(N)'/))
+    GHCTEMPFILES=(*.(hs|lhs)(N:r:s/%/'.(hi|hc|(p|u|s)_(o|hi))(N)'/))
+    PYTEMPFILES=(*.py(N:s/%py/'(pyc|pyo)(N)'/))
+    LONELY_MOOD_FILES=((*.mood)(NDe:'local -a AF;AF=( ${${REPLY#.}%mood}(mp3|flac|ogg|asf|wmv|aac)(N) ); [[ -z "$AF" ]]':))
+    FILES=(*~(.N) \#*\#(.N) *.o(.N) a.out(.N) (*.|)core(.N) *.cmo(.N) *.cmi(.N) .*.swp(.N) *.(orig|rej)(.DN) *.dpkg-(old|dist|new)(DN) ._(cfg|mrg)[0-9][0-9][0-9][0-9]_*(N) ${~TEXTEMPFILES} ${~GHCTEMPFILES} ${~PYTEMPFILES} ${LONELY_MOOD_FILES})
+    local NBFILES=${#FILES}
+    local CURDIRSUDO=""
+    [[ ! -w ./ ]] && CURDIRSUDO=$SUDO
+    if [[ $NBFILES > 0 ]] ; then
+        print -l $FILES
+        local ans
+        echo -n "Remove these files? [y/n] "
+        read -q ans
+        if [[ $ans == "y" ]] ; then
+            $CURDIRSUDO rm ${FILES}
+            echo ">> $PWD purged, $NBFILES files removed"
+        else
+            echo "Ok. .. then not.."
         fi
     fi
-} #}}}
+}
+#}}}
 
 # vim:filetype=sh textwidth=80 foldmethod=marker
