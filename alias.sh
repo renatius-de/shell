c81d71bef106ac74987d9baf01c60afca0819064
René Six <mail@rene-six.de>
2011-11-19 17:18:43 +0100

    add alias for ssh connection to laptop pandora

--- alias.sh
+++ alias.sh
@@ -13,6 +13,7 @@ if [[ -x /usr/bin/ssh ]]; then
     # rename ssh connections for details see ~/.ssh/config
     alias gobeteigeuze="ssh beteigeuze"
     alias goeuropa="ssh europa"
+    alias gopandora="ssh pandora"
     alias goserver="ssh server"
 
     # these machines are not under my controll
