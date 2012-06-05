dffd2d4812bea1a8bd544913cafaf7dd83521349
René Six <mail@rene-six.de>
2012-06-06 01:42:34 +0200

    update file shell/variable.sh in repository on date 2012-06-06

--- variable.sh
+++ variable.sh
@@ -5,7 +5,8 @@
 # in a hash table.
 [[ -d /usr/share/surfraw ]] && PATH=${PATH}:/usr/share/surfraw
 [[ -d ~/bin ]]              && PATH=${PATH}:~/bin
-export PATH #}}}
+export PATH
+#}}}
 
 # {{{ STARTUP PROMPT
 # using "birthday" and "calendar" to inform me about
@@ -23,26 +24,30 @@ builtin echo "------------------------------------------------------------------
 
 # {{{ EDITOR
 # setting the EDITOR environment
-export EDITOR="${EDITOR:-vim}" #}}}
+export EDITOR="${EDITOR:-vim}"
+#}}}
 
 # {{{ PAGER
 # setting the PAGER environment
-if [[ -x ~/.dotfiles/vim/bundle/VimPager/vimpager ]]; then
-    export PAGER=~/.dotfiles/vim/bundle/VimPager/vimpager
+if [[ -x ~/.vim/bundle/VimPager/vimpager ]]; then
+    export PAGER=~/.vim/bundle/VimPager/vimpager
 elif [[ -x /usr/bin/vimpager ]]; then
     export PAGER="vimpager"
 elif [[ -x /usr/share/vim/vimcurrent/macros/less.sh ]]; then
     export PAGER="/usr/share/vim/vimcurrent/macros/less.sh"
-fi  #}}}
+fi 
+#}}}
 
 # {{{ VISUAL
 # setting the VISUAL environment to the same es EDITOR
-export VISUAL="${EDITOR:-vim}" #}}}
+export VISUAL="${EDITOR:-vim}"
+#}}}
 
 # {{{ MANWDTH
 # setting the textwidth of manpages to 80 symbols, this make manpages more
 # readable
-export MANWIDTH="72" #}}}
+export MANWIDTH="72"
+#}}}
 
 # {{{ MANOPT
 # If ${MANOPT} is set, it will be parsed prior to man’s command line and is
@@ -51,17 +56,19 @@ export MANWIDTH="72" #}}}
 # candidates for being included in ${MANOPT} it is expected that they will become
 # obsolete. N.B. All spaces that should be interpreted as part of an option’s
 # argument must be escaped.
-export MANOPT="--locale='C'" #}}}
+export MANOPT="--locale='C'"
+#}}}
 
 # {{{ MANPAGER
 # If MANPAGER is set, its value is used as the name of the program to use to
 # display the man page. If not, then PAGER is used. If that has no value
 # either, /usr/bin/less -is is used.
-if [[ -x ~/.dotfiles/shell/vimmanpager ]]; then
-    export MANPAGER=~/.dotfiles/shell/vimmanpager
+if [[ -x ~/.shell/vimmanpager ]]; then
+    export MANPAGER=~/.shell/vimmanpager
 elif [[ -x /usr/bin/vimmanpager ]]; then
     export MANPAGER="vimmanpager"
-fi #}}}
+fi
+#}}}
 
 # {{{ BROWSER
 if [[ -x /usr/bin/lynx ]]; then
@@ -70,43 +77,23 @@ elif [[ -x /usr/bin/links ]]; then
     export BROWSER=/usr/bin/links
 elif [[ -x /usr/bin/elinks ]]; then
     export BROWSER=/usr/bin/elinks
-fi #}}}
-
-# {{{ PERLDOC
-# Any switches in the "PERLDOC" environment variable will be used before the
-# command line arguments.
-#
-# Useful values for "PERLDOC" include "-oman", "-otext", "-otk", "-ortf",
-# "-oxml", and so on, depending on what modules you have on hand; or exactly
-# specify the formatter class with "-MPod::Perldoc::ToMan" or the like.
-#
-# "perldoc" also searches directories specified by the "PERL5LIB" (or "PERLLIB"
-# if "PERL5LIB" is not defined) and "PATH" environment variables. (The latter is
-# so that embedded pods for executables, such as "perldoc" itself, are
-# available.)
-export PERLDOC="${PERLDOC} -oman -otext -otk -ortf -oxml -MPod::Perldoc::ToMan"
+fi
 #}}}
-
-# {{{ PERLDOC_PAGER
-# "perldoc" will use, in order of preference, the pager defined in
-# "PERLDOC_PAGER", "MANPAGER", or "PAGER" before trying to find a pager on its
-# own. ("MANPAGER" is not used if "perldoc" was told to display plain text or
-# unformatted pod.)
-#
-# One useful value for "PERLDOC_PAGER" is "less -+C -E".
-export PERLDOC_PAGER="less -+C -E" #}}}
 
 # {{{ GREP_COLOR
 # give the output of grep a more usefull lock
-export GREP_COLOR="31;01" #}}}
+export GREP_COLOR="31;01"
+#}}}
 
 # {{{ GREP_OPTIONS
 # additional options for grep
-export GREP_OPTIONS="--color=auto" #}}}
+export GREP_OPTIONS="--color=auto"
+#}}}
 
 # {{{ CDDB_PATH
 # settings for mcdp
-export CDDB_PATH="~/.cddb/" #}}}
+export CDDB_PATH="~/.cddb/"
+#}}}
 
 # {{{ GZIP BZIP2 LZMA XZ
 # default options for gzip, bzip2, lzma and xz
@@ -119,11 +106,13 @@ export CDDB_PATH="~/.cddb/" #}}}
 # {{{ TAR_OPTIONS
 # The TAR_OPTIONS environment variable specifies default options to be placed in
 # front of any explicit options.
-export TAR_OPTIONS="--auto-compress --delay-directory-restore --exclude-backups --exclude-caches --no-overwrite-dir --numeric-owner --totals" #}}}
+export TAR_OPTIONS="--auto-compress --delay-directory-restore --exclude-backups --exclude-caches --no-overwrite-dir --numeric-owner --totals"
+#}}}
 
 # {{{ TZ
 # timzone settings for Europe/Berlin
-export TZ="Europe/Berlin" #}}}
+export TZ="Europe/Berlin"
+#}}}
 
 # {{{ GPG_TTY
 # You should always add the following lines to your .bashrc or whatever
@@ -132,15 +121,13 @@ export TZ="Europe/Berlin" #}}}
 # export GPG_TTY
 # It is important that this environment variable always reflects the output of
 # the tty command. For W32 systems this option is not required.
-export GPG_TTY="${TTY:-`tty`}" #}}}
+export GPG_TTY="${TTY:-`tty`}"
+#}}}
 
 # {{{ SUDO_PROMPT
 # Used as the default password prompt.
-[[ -x /usr/bin/sudo ]] && export SUDO_PROMPT="[sudo] password for %p:" #}}}
-
-# {{{ XSESSION
-# start this session with startx
-export XSESSION="fluxbox" #}}}
+[[ -x /usr/bin/sudo ]] && export SUDO_PROMPT="[sudo] password for %p:"
+#}}}
 
 # {{{ DVDCSS_CACHE
 # Specify a directory in which to store title key values. This will speed up
@@ -150,13 +137,15 @@ export XSESSION="fluxbox" #}}}
 # libdvdcss will use the default value which is "${HOME}/.dvdcss/" under Unix
 # and "C:\Documents and Settings\$USER\Application Data\dvdcss\" under Win32.
 # The special value "off" disables caching.
-export DVDCSS_CACHE="off" #}}}
+export DVDCSS_CACHE="off"
+#}}}
 
 # {{{ DVDCSS_VERBOSE
 # Sets the libdvdcss verbosity level.
 #   0   Outputs no messages at all.
 #   1   Outputs error messages to stderr.
 #   2   Outputs error messages and debug messages to stderr.
-export DVDCSS_VERBOSE=1 #}}}
+export DVDCSS_VERBOSE=1
+#}}}
 
 # vim:filetype=sh textwidth=80 foldmethod=marker
