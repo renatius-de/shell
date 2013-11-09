63444d60d54be4c396ed8d231501065e887be1f5
René Six <mail@rene-six.de>
2013-11-09 18:33:05 +0100

    update file export.sh in repository on date 2013-11-09
    Signed-off-by: René Six <mail@rene-six.de>

--- export.sh
+++ export.sh
@@ -59,4 +59,21 @@ export GPG_TTY="${TTY:-`tty`}"
 export TZ="Europe/Berlin"
 #}}}
 
+# {{{ language variables
+export LANG=en_US.utf8
+export LC_CTYPE=en_US.utf8
+export LC_NUMERIC=en_US.utf8
+export LC_TIME=en_US.utf8
+export LC_COLLATE=en_US.utf8
+export LC_MONETARY=en_US.utf8
+export LC_MESSAGES=en_US.utf8
+export LC_PAPER=en_US.utf8
+export LC_NAME=en_US.utf8
+export LC_ADDRESS=en_US.utf8
+export LC_TELEPHONE=en_US.utf8
+export LC_MEASUREMENT=en_US.utf8
+export LC_IDENTIFICATION=en_US.utf8
+export LC_ALL=
+#}}}
+
 # vim:filetype=sh textwidth=80 foldmethod=marker
