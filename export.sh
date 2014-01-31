28793e5834d3189681b88996e3a7ee2b5fa20e1d
René Six <mail@rene-six.de>
2014-02-01 00:20:01 +0100

    update file export.sh in repository on date 2014-02-01
    Signed-off-by: René Six <mail@rene-six.de>

--- export.sh
+++ export.sh
@@ -1,3 +1,9 @@
+# {{{ test for an interactive shell
+case $- in
+    !*i*) return ;;
+esac
+#}}}
+
 # {{{ HOSTNAME
 [ -z "${HOSTNAME}" ] && HOSTNAME="$(uname -n)"
 #}}}
@@ -60,19 +66,19 @@ export TZ="Europe/Berlin"
 #}}}
 
 # {{{ language variables
-export LANG=en_US.utf8
-export LC_CTYPE=en_US.utf8
-export LC_NUMERIC=en_US.utf8
-export LC_TIME=en_US.utf8
-export LC_COLLATE=en_US.utf8
-export LC_MONETARY=en_US.utf8
-export LC_MESSAGES=en_US.utf8
-export LC_PAPER=en_US.utf8
-export LC_NAME=en_US.utf8
-export LC_ADDRESS=en_US.utf8
-export LC_TELEPHONE=en_US.utf8
-export LC_MEASUREMENT=en_US.utf8
-export LC_IDENTIFICATION=en_US.utf8
+export LANG=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
+export LC_NUMERIC=en_US.UTF-8
+export LC_TIME=en_US.UTF-8
+export LC_COLLATE=en_US.UTF-8
+export LC_MONETARY=en_US.UTF-8
+export LC_MESSAGES=en_US.UTF-8
+export LC_PAPER=en_US.UTF-8
+export LC_NAME=en_US.UTF-8
+export LC_ADDRESS=en_US.UTF-8
+export LC_TELEPHONE=en_US.UTF-8
+export LC_MEASUREMENT=en_US.UTF-8
+export LC_IDENTIFICATION=en_US.UTF-8
 export LC_ALL=
 #}}}
 
