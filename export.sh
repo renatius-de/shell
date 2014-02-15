15781c1d0b88d922b3f433b161a946fbfe09703e
René Six <info@renatius.de>
2014-02-15 19:48:30 +0100

    update file export.sh in repository on date 2014-02-15
    Signed-off-by: René Six <info@renatius.de>

--- export.sh
+++ export.sh
@@ -66,7 +66,6 @@ export TZ="Europe/Berlin"
 #}}}
 
 # {{{ language variables
-export LANG=en_US.UTF-8
 export LC_CTYPE=en_US.UTF-8
 export LC_NUMERIC=en_US.UTF-8
 export LC_TIME=en_US.UTF-8
@@ -80,6 +79,8 @@ export LC_TELEPHONE=en_US.UTF-8
 export LC_MEASUREMENT=en_US.UTF-8
 export LC_IDENTIFICATION=en_US.UTF-8
 export LC_ALL=
+
+export LANG=en_US
 #}}}
 
 # vim: filetype=sh textwidth=80 foldmethod=marker
