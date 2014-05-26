a6638c0c0ad11e4f3808cb6a72e1c79b04eb8f9e
René Six <info@renatius.de>
2014-05-26 14:57:07 +0200

    remove deprecated function

--- function.sh
+++ function.sh
@@ -4,26 +4,6 @@ case $- in
 esac
 #}}}
 
-# {{{ build packages from AUR for arch linux
-function buildArchPackages() {
-    local repo=~/norepositories/arch-aur
-    local curpath="$(pwd)"
-    cd ${repo}
-    git fm
-    git re
-    git cf master
-    git total-clean
-    git ri
-
-    for i in $(pacman -Qqm); do
-        cd "${repo}"/${i}
-        makepkg --check --clean --install --log --needed --noconfirm --syncdeps
-    done
-
-    cd ${curpath}
-}
-#}}}
-
 # {{{ reload configuration
 function reload() {
     if [ -n ${ZSH_NAME} ]; then
