50a08d4455e3fdc2510b0b188b66afafcf03df88
René Six <info@renatius.de>
2015-01-04 22:32:56 +0100

    update file export.sh in repository on date 2015-01-04

--- export.sh
+++ export.sh
@@ -25,8 +25,8 @@ fi
 # prompt. If there is a trap on SIGALRM, it will be executed and a new alarm is
 # scheduled using the value of the TMOUT parameter after executing the trap. If
 # no trap is set, and the idle time of the terminal is not less than the value
-# of the TMOUT parameter, zsh terminates. Otherwise a new alarm is scheduled to
-# TMOUT seconds after the last keypress.
+# of the TMOUT parameter, shell terminates. Otherwise a new alarm is scheduled
+# to TMOUT seconds after the last key press.
 if [[ ${TERM} = linux ]]; then
     export TMOUT="3600"
 else
@@ -118,6 +118,9 @@ export TAR_OPTIONS="--auto-compress --delay-directory-restore --exclude-backups 
 
 # should be in system path, but isn't in Max OS X
 [ -d /usr/local/bin ]        && PATH="/usr/local/bin:${PATH}"
+
+# add local rubygems installs
+[ -d ~/.gem/ruby/2.1.0/bin ] && PATH="~/.gem/ruby/2.1.0/bin:${PATH}"
 
 # set PATH so it includes user's private bin if it exists
 [ -d ${HOME}/bin ]           && PATH="${HOME}/bin:${PATH}"
