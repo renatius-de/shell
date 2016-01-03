ab73a555c5c82ca803f37217bb811597646656fe
René Six <info@renatius.de>
2016-01-03 18:50:30 +0100

    update file alias.sh in repository on date 2016-01-03

--- alias.sh
+++ alias.sh
@@ -278,6 +278,7 @@ fi
 
 # {{{ default options for phpunit
 if $(which php > /dev/null 2>&1); then
+    alias php='php -d "date.timezone=Europe/Berlin"'
     alias behat='php -d "error_reporting=E_ALL|E_STRICT" -d "memory_limit=256M" ./bin/behat --format progress'
     alias phpunit='php -d "error_reporting=E_ALL|E_STRICT" -d "memory_limit=256M" ./bin/phpunit --colors --verbose'
 fi
