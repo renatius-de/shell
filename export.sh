137794295437052de9f5fb76ada125f873f80320
René Six <info@renatius.de>
2016-05-14 07:18:42 +0200

    update file export.sh in repository on date 2016-05-14

--- export.sh
+++ export.sh
@@ -132,21 +132,21 @@ export TZ="Europe/Berlin"
 #}}}
 
 # {{{ language variables
-export LC_CTYPE=en_US.UTF-8
-export LC_NUMERIC=en_US.UTF-8
-export LC_TIME=de_DE.UTF-8
-export LC_COLLATE=en_US.UTF-8
-export LC_MONETARY=en_US.UTF-8
-export LC_MESSAGES=en_US.UTF-8
-export LC_PAPER=de_DE.UTF-8
-export LC_NAME=en_US.UTF-8
-export LC_ADDRESS=en_US.UTF-8
-export LC_TELEPHONE=en_US.UTF-8
-export LC_MEASUREMENT=de_DE.UTF-8
-export LC_IDENTIFICATION=en_US.UTF-8
+export LC_CTYPE=en_US.utf8
+export LC_NUMERIC=en_US.utf8
+export LC_TIME=de_DE.utf8
+export LC_COLLATE=en_US.utf8
+export LC_MONETARY=en_US.utf8
+export LC_MESSAGES=en_US.utf8
+export LC_PAPER=de_DE.utf8
+export LC_NAME=en_US.utf8
+export LC_ADDRESS=en_US.utf8
+export LC_TELEPHONE=en_US.utf8
+export LC_MEASUREMENT=de_DE.utf8
+export LC_IDENTIFICATION=en_US.utf8
 export LC_ALL=
 
-export LANG=en_US.UTF-8
+export LANG=en_US.utf8
 
 export LANGUAGE=en_GB:en_US:en:de_DE:de
 #}}}
