8a1eac512f00fe50b98c89db7113c8b7deb0e440
René Six <info@renatius.de>
2016-06-09 14:58:57 +0200

    update file alias.sh in repository on date 2016-06-09

--- alias.sh
+++ alias.sh
@@ -207,7 +207,7 @@ fi
 # {{{ default options for phpunit
 if $(which php > /dev/null 2>&1); then
     alias psp='php -d "date.timezone=UTC" -d "error_reporting=E_ALL|E_STRICT" -d "memory_limit=1G"'
-    alias behat='php ./bin/behat --format progress'
+    alias behat='php ./bin/behat --verbose'
     alias phpunit='php ./bin/phpunit --no-coverage --colors --verbose'
 fi
 #}}}
