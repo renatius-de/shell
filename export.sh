4d979fa5abc1cacaed7933f304088988bc90c664
René Six <info@renatius.de>
2017-01-11 12:38:39 +0100

    start gpg agent with keychain as well

--- export.sh
+++ export.sh
@@ -87,7 +87,7 @@ export TZ="Europe/Berlin"
 #}}}
 
 # {{{ add SSH and GPG agent
-hash keychain >> /dev/null 2>&1 && eval $(keychain --agents ssh --eval --quiet)
+hash keychain >> /dev/null 2>&1 && eval $(keychain --eval --quiet)
 #}}}
 
 # {{{ language variables
