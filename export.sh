4625a45047e8dca1d09ef56d7dbab9adbb1c5dd1
René Six <info@renatius.de>
2018-12-06 08:01:18 +0100

    Correct language in exported variables

--- export.sh
+++ export.sh
@@ -81,18 +81,19 @@ which keychain > /dev/null 2>&1 && eval $(keychain --agents gpg,ssh --eval --qui
 #}}}
 
 # {{{ language variables
-export LC_CTYPE=en_US.UTF-8
-export LC_NUMERIC=en_US.UTF-8
-export LC_TIME=de_DE.UTF-8
-export LC_COLLATE=en_US.UTF-8
-export LC_MONETARY=en_US.UTF-8
-export LC_MESSAGES=POSIX
-export LC_PAPER=de_DE.UTF-8
-export LC_NAME=en_US.UTF-8
 export LC_ADDRESS=en_US.UTF-8
-export LC_TELEPHONE=en_US.UTF-8
-export LC_MEASUREMENT=de_DE.UTF-8
+export LC_COLLATE=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
 export LC_IDENTIFICATION=en_US.UTF-8
+export LC_MEASUREMENT=de_DE.UTF-8
+export LC_MESSAGES=POSIX
+export LC_MESSAGES=en_US.UTF-8
+export LC_MONETARY=en_US.UTF-8
+export LC_NAME=en_US.UTF-8
+export LC_NUMERIC=en_US.UTF-8
+export LC_PAPER=de_DE.UTF-8
+export LC_TELEPHONE=en_US.UTF-8
+export LC_TIME=de_DE.UTF-8
 unset LC_ALL
 
 export LANG=en_US.UTF-8
